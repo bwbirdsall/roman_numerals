830398ff72c885624330ff8a9a5ef092c109bffc
Ben Birdsall & Alex Wood <student@epicodus.com>
2014-03-05 14:58:49 -0800

    adds gem monkeypatch on integer

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,23 +1,26 @@
-def roman_numerals(number) 
-  numeral_letters = ""
-  new_letter = ""
-  roman_pairs = {1000 => "M", 
-                  900 => "CM",
-                  500 => "D",
-                  400 => "CD",
-                  100 => "C",
-                  90 => "XC",
-                  50 => "L",
-                  40 => "XL",
-                  10 => "X",
-                  9 => "IX",
-                  5 => "V",
-                  4 => "IV",
-                  1 => "I"}
-  while (number > 0)
-    biggest_pair = roman_pairs.select{|k,v| number >= k}.max_by{|k,v| k}
-    numeral_letters += biggest_pair[1]
-    number = number - biggest_pair[0]
+class Integer
+  def rn 
+    number = self
+    numeral_letters = ""
+    new_letter = ""
+    roman_pairs = {1000 => "M", 
+                    900 => "CM",
+                    500 => "D",
+                    400 => "CD",
+                    100 => "C",
+                    90 => "XC",
+                    50 => "L",
+                    40 => "XL",
+                    10 => "X",
+                    9 => "IX",
+                    5 => "V",
+                    4 => "IV",
+                    1 => "I"}
+    while (number > 0)
+      biggest_pair = roman_pairs.select{|k,v| number >= k}.max_by{|k,v| k}
+      numeral_letters += biggest_pair[1]
+      number = number - biggest_pair[0]
+    end
+    numeral_letters
   end
-  numeral_letters
 end
---
+++ roman_numerals.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |s|
+  s.name = "roman_numerals"
+  s.version = "0.0.0"
+  s.date = "2014-03-05"
+  s.summary = "Roman Numeral Converter"
+  s.description = "Converts inferior Arabic numerals to superior Roman ones."
+  s.authors = ["Alex Wood", "Ben Birdsall"]
+  s.files = ["lib/roman_numerals.rb"]
+end
